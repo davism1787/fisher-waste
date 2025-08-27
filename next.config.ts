052f716bc69da1de8a3fd45a626655984b335f9c
Matt Davis <davism1787@gmail.com>
2025-08-27 11:46:59 -0500

    telling nextjs to skip supabase building

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,16 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  webpack: (config) => {
+    config.watchOptions = {
+      ...config.watchOptions,
+      ignored: ['**/supabase/**'],
+    };
+    return config;
+  },
+  experimental: {
+    serverComponentsExternalPackages: [],
+  },
 };
 
 export default nextConfig;
